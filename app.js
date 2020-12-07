45175906337f8f524a2bee10b97417f2da87c7c1
Phos23 <phos23@gmail.com>
2020-12-07 21:08:15 +0000

    add example stuff to app.js

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('Request received!');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
 
-app.use((req, res, next) => {
-    res.json({
-        message: 'Your request was successful',
-    });
-    next();
-});
 
-app.use((req, res, next) => {
-    console.log('Response sent successfully');
-})
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [{
+        _id: 'string',
+        title: 'My first thing',
+        description: 'All of the info about my first thing',
+        imageUrl: '',
+        price: 4900,
+        userId: 'asdsdgfdfg'
+    }, {
+        _id: 'strings',
+        title: 'My second thing',
+        description: 'All of the info about my second thing',
+        imageUrl: '',
+        price: 2900,
+        userId: 'asdsdgfdfg'
+    }];
+    res.status(200).json(stuff);
+});
 
 module.exports = app;
