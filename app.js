9840d99735cf1819b904f372488fad727c1ca6f4
Phos23 <phos23@gmail.com>
2020-12-13 17:53:51 +0000

    add login routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/stuff', stuffRoutes)
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes)
 
 module.exports = app;
