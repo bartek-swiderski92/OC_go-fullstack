5c69be16835697e5978c0016a4b138752c916472
Phos23 <phos23@gmail.com>
2020-12-13 18:44:38 +0000

    add signup logic

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes)
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
