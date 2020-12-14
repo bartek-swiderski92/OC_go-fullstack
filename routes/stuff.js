808e489dd5c6df9d0cd598d82e68372fed9f4f12
Phos23 <phos23@gmail.com>
2020-12-14 21:26:55 +0000

    add images uploading to createThing

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-const stuffCtrl = require('../controllers/stuff')
+const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', auth, stuffCtrl.createThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.put('/:id', auth, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
-router.get('/', auth, stuffCtrl.getAllStuff);
 
 module.exports = router;
