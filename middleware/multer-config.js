3d0210947449e619e2fc96cb0760dfd73811e70d
Phos23 <phos23@gmail.com>
2020-12-20 19:20:16 +0000

    fix typo in multer config

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,8 +1,8 @@
 const multer = require('multer');
 
 const MIME_TYPES = {
-    'image/jpg:': 'jpg',
-    'image/jpeg:': 'jpg',
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
     'image/png:': 'png',
 };
 
